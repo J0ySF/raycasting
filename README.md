7bdf32a4b77d10241064b64657d0263c841ca86c
Joy Spuri Forotti <joyspuriforotti@gmail.com>
2023-05-11 20:53:21 +0200

    Added emscripten support

--- README.md
+++ README.md
@@ -2,31 +2,37 @@
 
 ## Dependencies
 
-Build system: `cmake`.
+Build system: `cmake`
 
-Libraries: `SDL2`.
+Libraries: `SDL2`
 
 ## Supported platforms
 
 ### Linux
 
-Build program by running `cmake -B .linux && cmake --build .linux`.
+Requires `cmake` and the `SDL2` development package to be installed.
 
-Run via `.linux/raycasting`.
++ Build program by running `cmake -B .linux && cmake --build .linux`
+
++ Run via `.linux/raycasting`
 
 ### Flatpak
 
-Requires `flatpak` and `flatpak-builder` to be installed.
+Requires `cmake`, `flatpak` and `flatpak-builder` to be installed.
 
-Install flatpak dependencies by running `flatpak install flathub org.freedesktop.Platform//22.08 org.freedesktop.Sdk//22.08`.
++ Install flatpak dependencies by running `flatpak install flathub org.freedesktop.Platform//22.08 org.freedesktop.Sdk//22.08`
 
-Build program by running `flatpak-builder --force-clean .flatpak pkg/it.joysf.raycasting.yml`.
++ Build program by running `flatpak-builder --force-clean .flatpak pkg/it.joysf.raycasting.yml`
 
-Run via `flatpak-builder --run .flatpak .flatpak/files/manifest.json raycasting`.
++ Run via `flatpak-builder --run .flatpak .flatpak/files/manifest.json raycasting`
 
 ### Emscripten
 
-Planned in the future.
+Requires `cmake` the `emscripten` toolchain to be installed.
+
++ Build webpage by running `emcmake cmake -B .emscripten -DCMAKE_BUILD_TYPE=Release && emmake cmake --build .emscripten`
+
++ Run via `emrun .emscripten/raycasting.html`
 
 ## Assets
 
--- src/main.c
+++ src/main.c
@@ -1,9 +1,55 @@
 #include "config.h"
 
+#ifndef EMSCRIPTEN
+
 #include <SDL2/SDL.h>
 
-SDL_Window *window;
-SDL_Renderer *renderer;
+#else
+
+#include <SDL.h>
+#include <emscripten.h>
+#include <emscripten/html5.h>
+
+#endif
+
+static SDL_Window *window;
+static SDL_Renderer *renderer;
+
+#ifdef EMSCRIPTEN
+
+EM_BOOL emscripten_canvas_resized_callback(int, const void *, void *) {
+    int width, height;
+    emscripten_get_canvas_element_size("canvas", &width, &height);
+    SDL_SetWindowSize(window, width, height);
+    return true;
+}
+
+int loop();
+
+void emscripten_loop(void *) {
+    if(!loop()) emscripten_cancel_main_loop();
+}
+
+#endif
+
+int loop() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                return 0;
+        }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawLine(renderer, 0, 0, PRESENT_WIDTH, PRESENT_HEIGHT);
+    SDL_RenderDrawLine(renderer, PRESENT_WIDTH, 0, 0, PRESENT_HEIGHT);
+    SDL_RenderPresent(renderer);
+
+    return 1;
+}
 
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO)) {
@@ -11,7 +57,8 @@ int main() {
         return 1;
     }
 
-    window = SDL_CreateWindow(PROGRAM_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, PRESENT_WIDTH, PRESENT_HEIGHT, SDL_WINDOW_RESIZABLE);
+    window = SDL_CreateWindow(PROGRAM_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, PRESENT_WIDTH,
+                              PRESENT_HEIGHT, SDL_WINDOW_RESIZABLE);
     if (window == NULL) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_CreateWindow failed: %s", SDL_GetError());
         return 1;
@@ -28,24 +75,22 @@ int main() {
         return 1;
     }
 
-    while(1) {
-        SDL_Event event;
-        while(SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT:
-                    goto Quit;
-            }
-        }
+#ifndef EMSCRIPTEN
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderDrawLine(renderer, 0, 0, PRESENT_WIDTH, PRESENT_HEIGHT);
-        SDL_RenderDrawLine(renderer, PRESENT_WIDTH, 0, 0, PRESENT_HEIGHT);
-        SDL_RenderPresent(renderer);
-    }
+    while (loop()) {}
 
-    Quit:
+#else
+
+    EmscriptenFullscreenStrategy strategy;
+    strategy.scaleMode =  EMSCRIPTEN_FULLSCREEN_SCALE_STRETCH;
+    strategy.canvasResolutionScaleMode = EMSCRIPTEN_FULLSCREEN_CANVAS_SCALE_HIDEF,
+    strategy.filteringMode = EMSCRIPTEN_FULLSCREEN_FILTERING_DEFAULT;
+    strategy.canvasResizedCallback = emscripten_canvas_resized_callback;
+    emscripten_enter_soft_fullscreen("canvas", &strategy);
+    emscripten_set_main_loop_arg(emscripten_loop, NULL, 0, true);
+
+#endif
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
